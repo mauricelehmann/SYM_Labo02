ababa4c1f52a745fc0fb245d52e7a8f51911195c
Maurice <m.lehmann94@gmail.com>
2020-10-23 16:10:54 +0200

    init. project

---
+++ app/src/main/java/com/heig/labo02/comm/CommunicationEventListener.kt
@@ -0,0 +1,5 @@
+package com.heig.labo02.comm
+
+interface CommunicationEventListener {
+    fun handleServerResponse(response :String)
+}
---
+++ app/src/main/java/com/heig/labo02/comm/SymComManager.kt
@@ -0,0 +1,9 @@
+package com.heig.labo02.comm
+
+class SymComManager(var communicationEventListener: CommunicationEventListener? = null) {
+
+    fun sendRequest(url: String, request: String) {
+		//TODO to implement
+    }
+
+}

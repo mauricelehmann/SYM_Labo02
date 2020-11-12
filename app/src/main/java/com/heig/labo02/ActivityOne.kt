4585d14a932a62bdabde3f4930cc0924f981b8ee
mauricelehmann <m.lehmann94@gmail.com>
2020-11-12 10:48:36 +0100

    empty strucutre of activityOne

--- app/src/main/java/com/heig/labo02/ActivityOne.kt
+++ app/src/main/java/com/heig/labo02/ActivityOne.kt
@@ -1,5 +1,26 @@
 package com.heig.labo02
 
-class ActivityOne {
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class ActivityOne : AppCompatActivity() {
+
+    private lateinit var activityOneButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // l'appel à la méthode onCreate de la super classe est obligatoire
+        super.onCreate(savedInstanceState)
+        // on définit le layout à utiliser pour l'affichage
+        setContentView(R.layout.activity_one)
+
+        activityOneButton = findViewById(R.id.activity_one_button)
+        activityOneButton.setOnClickListener {
+          // Send http req here
+            val toast = Toast.makeText(applicationContext, "Welcome ", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+    }
 
 }

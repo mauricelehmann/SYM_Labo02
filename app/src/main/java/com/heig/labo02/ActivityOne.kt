fd03b0ae2537bb63b119cef5886716f7cd007cba
quillasp <saralfddin93@yahoo.fr>
2020-11-12 22:51:08 +0100

    Activité 1 fonctionnelle avec thread

--- app/src/main/java/com/heig/labo02/ActivityOne.kt
+++ app/src/main/java/com/heig/labo02/ActivityOne.kt
@@ -1,7 +1,6 @@
 package com.heig.labo02
 
-import android.os.Build
-import android.os.Bundle
+import android.os.*
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -9,6 +8,7 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.heig.labo02.comm.CommunicationEventListener
 import com.heig.labo02.comm.SymComManager
+import kotlin.concurrent.thread
 
 class ActivityOne : AppCompatActivity() {
 
@@ -25,19 +25,44 @@ class ActivityOne : AppCompatActivity() {
         val responseBox = findViewById<TextView>(R.id.activity_one_text)
         activityOneButton = findViewById(R.id.activity_one_button)
 
-
         // Definition du listener du communication manager
-        comManager.communicationEventListener = object : CommunicationEventListener {
-            override fun handleServerResponse(response: String): Boolean {
-                responseBox.setText(response)
-                return true
+//        comManager.communicationEventListener = object : CommunicationEventListener {
+//            override fun handleServerResponse(response: String): Boolean {
+//                responseBox.text = response
+//                return true
+//            }
+//        }
+
+        val handler = object : Handler(Looper.getMainLooper()) {
+            override fun handleMessage(msg: Message) {
+                val toast = Toast.makeText(applicationContext, "is dis answer ?", Toast.LENGTH_SHORT)
+                toast.show()
+                responseBox.text = msg.data.getString("FROM_SERVER")
             }
         }
+
         activityOneButton.setOnClickListener {
             // Send http req here
             val toast = Toast.makeText(applicationContext, "Yee", Toast.LENGTH_SHORT)
             toast.show()
-            comManager.sendRequest( "https://sym.iict.ch/rest/txt", "toto");
+            responseBox.text = "En attente de la réponse"
+            // comManager.sendRequest( "http://sym.iict.ch/rest/txt", "toto");
+
+            thread (start = true) {
+                val mcm = com.heig.labo02.comm.SymComManager(
+                object : CommunicationEventListener {
+                    override fun handleServerResponse(response: String): Boolean {
+                        val msg: Message = handler.obtainMessage()
+                        var b = Bundle()
+                        b.putString("FROM_SERVER", response)
+                        msg.data = b
+                        handler.sendMessage(msg)
+                        return true
+                    }
+                }
+            )
+            mcm.sendRequest( "http://sym.iict.ch/rest/txt", "i wuv kt")
+            }
         }
     }
     companion object {
